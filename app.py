2672489a31a0024756f72116ff0b8506f0a89198
Angelo Gallardi <angelogallardi@hotmail.com>
2025-02-23 20:59:21 -0300

    finalizados el formateo de lo copiado y su verificacion

---
+++ app.py
@@ -0,0 +1,113 @@
+import pyperclip
+import re
+
+# Indices de cada columna
+colToInd = {
+	'ot': 0,
+	'fecha': 1,
+	'solicitante': 2,
+	'sector': 3,
+	'descripcion': 4,
+	'entrega': 5,
+	'siniva': 6,
+	'coniva': 7
+}
+
+
+def formatClipboard():
+	""" Formatea el texto del portapapeles para obtener la tabla en un formato manejable. """
+
+	# Obtengo el contenido del portapapeles
+	cb = pyperclip.paste()
+
+	# Reemplazo saltos de línea dentro de las celdas por un espacio
+	cb = re.sub(r'(\t".*?"\t)', lambda m: m.group(1).replace('\n', ' '), cb, flags=re.DOTALL)
+
+	# Quito las comillas dobles que se agregan al texto de celdas con saltos de linea
+	cb = re.sub(r'(?<=\t)"', '', cb) # Lookbehind positivo
+	cb = re.sub(r'"(?=\t)', '', cb) # Lookahead positivo
+
+	# Divido en filas y columnas
+	rows = cb.splitlines() # Separo en filas
+	table = [row.split('\t') for row in rows] # Separo en columnas
+
+	# # Imprimo la tabla
+	# for row in table:
+	# 	print(row)
+
+	return table
+
+
+def checkClipboard():
+	""" Verifica si lo copiado en el portapapeles es de la hoja de Google Sheets. """
+
+	# Obtengo la tabla formateada
+	table = formatClipboard()
+	# print(table)
+
+	# Verifico si hay texto en el portapapeles
+	if not table:
+		print('No se copió nada')
+		return False
+
+	# Verifico cantidad correcta de columnas (7 hasta "sin IVA", 8 hasta "con IVA")
+	for row in table:
+		if len(row) < 7 or len(row) > 8: 
+			print('Cantidad de columnas incorrecta')
+			return False
+
+	# Verifico contenido de cada columna
+	for row in table:
+		# Columna "OT"
+		ot = row[colToInd['ot']]
+		if not ot.isdecimal() and ot != '#N/A':
+			print(f'"OT" con formato inválido: {ot}')
+			return False
+
+		# Columna "Fecha"
+		fecha = row[colToInd['fecha']]
+		dateRegex = r'''
+			\d{4}[-/.]\d{1,2}[-/.]\d{1,2} # Captura fechas con el año primero
+			|							  # o...
+			\d{1,2}[-/.]\d{1,2}[-/.]\d{4} # Captura fechas con el año al final
+		'''
+		if not re.match(dateRegex, fecha, re.VERBOSE) and fecha != '#N/A':
+			print(f'"Fecha" con formato inválido: {fecha}')
+			return False
+
+		# Columna "Solicitado por"
+		solicitante = row[colToInd['solicitante']]
+		if not solicitante.isalpha() and len(solicitante) > 10 and solicitante != '#N/A':
+			print(f'"Solicitado por" con formato inválido: {solicitante}')
+			return False
+
+		# Columna "Sector"
+		sector = row[colToInd['sector']]
+		if not sector.isalpha() and len(sector) > 10 and sector != '#N/A':
+			print(f'"Sector" con formato inválido: {sector}')
+			return False
+
+		# Columnas "Descripción" y "Entrega" no las controlo
+
+		# Columna "Valor S/IVA"
+		siniva = row[colToInd['siniva']]
+		sinivaLimpio = siniva.replace('$', '').replace('.', '')
+		if not sinivaLimpio.isdecimal() and sinivaLimpio.count(',') != 1:
+			print(f'"Valor S/IVA" con formato inválido: {siniva}')
+			return False
+
+		# Columna "Valor c/IVA"
+		if len(row) == 8:
+			coniva = row[colToInd['coniva']]
+			conivaLimpio = coniva.replace('$', '').replace('.', '')
+			if not conivaLimpio.isdecimal() and conivaLimpio.count(',') != 1:
+				print(f'"Valor C/IVA" con formato inválido: {coniva}')
+				return False
+
+	print('TODO OK!')
+	return True
+
+
+
+while not checkClipboard():
+	pass
